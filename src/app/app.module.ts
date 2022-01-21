dcd57f4f2f063b142c47808ad7cbd46a29d35da4
Toni M <tonims.dev@gmail.com>
2022-01-21 18:14:13 +0100

    Finishing Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ItemComponent } from './features/home/components/main/item/item.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
